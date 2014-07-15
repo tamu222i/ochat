c1b19f70bcb05395e8a37191cfb0e45d6355cc25
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2014-07-15 13:12:42 +0000

    default data

--- ochat.js
+++ ochat.js
@@ -1,3 +1,8 @@
+Players = new Meteor.Collection('players');
+var player = function () {
+  return Players.findOne(Session.get('player_id'));
+};
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to ochat.";
@@ -10,6 +15,20 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
+Template.lobby.disabled = function () {
+  var me = player();
+console.log("aaa", me);
+
+  if (me && me.name)
+    return '';
+  return 'disabled';
+};
+  Meteor.startup(function () {
+  var player_id = Players.insert({name: '', idle: false});
+  Session.set('player_id', player_id);
+  });
+
 }
 
 if (Meteor.isServer) {
